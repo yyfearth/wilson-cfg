2a77056ebd6f843190ef8c9653a82f661c27e590
Wilson Young <yyfearth@gmail.com>
2012-10-16 03:13:07 -0700

    update mac settings
    add selectable in quick look
    quoted strings

--- cmd/mac_settings.sh
+++ cmd/mac_settings.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 
-echo Key Repeat
+echo "Enable Key Repeat"
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-echo Change Default Saving Place to Local Disk
+echo "Change Default Saving Place to Local Disk"
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
-echo Show All Files
+echo "Show All Files"
 defaults write com.apple.Finder AppleShowAllFiles YES
 
-echo Advanced Air Drop
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1 && killall Finder
+echo "Advanced Air Drop"
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
-echo Show Library
+echo "Text Selectable in Quick Look previews"
+defaults write com.apple.finder QLEnableTextSelection -bool TRUE
+
+echo "Show User Library"
 chflags nohidden ~/Library/
 
-echo Hide a volume in Mac OS X
-SetFile -a V /Volumes/Name
+# echo "Hide a volume in OSX"
+# SetFile -a V /Volumes/OSX\ Installer
 
-echo Sublime Test Cmd Line
+echo "Sublime Test Cmd Line"
 ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
 
 # echo Disable Hibernate
 # sudo pmset -a hibernatemode 0
 # sudo rm /var/vm/sleepimage
+
+echo "Kill Finder to Apply Settings"
+killall Finder
